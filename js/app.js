2a4ec247b16747b69955c0d76a6908a73c1f9c3f
AbdullahElian1 <abodelian28@gmail.com>
2021-04-11 14:29:10 +0300

    add numOfCookies function

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ const seattle ={
     max: 65,
     avg: 6.3,
     cusNum: [],
-    
+    calCookies:[],
+    totalCookies:0,
+
 
     getRandomInt: function (min, max) {
         for(let i =0; i<=13; i++){
@@ -19,10 +21,34 @@ const seattle ={
         console.log(this.cusNum);
     },
 
+    numOfCookies: function(){
+        for(let i=0; i<14; i++)
+        {
+
+            this.calCookies.push(this.cusNum[i]* this.avg);
+        }
+
+    },
+
+    totalNumOfCookies: function(){
+        for(let i=0; i<=13; i++){
+            this.totalCookies += this.calCookies[i];
+            
+
+        }
+        console.log(this.totalCookies);
+        
+
+    }
+
 
 
 };
 seattle.getRandomInt(23,65);
+seattle.numOfCookies();
+console.log(seattle.calCookies);
+seattle.totalNumOfCookies();
+console.log(seattle.totalCookies);
 
 
 
