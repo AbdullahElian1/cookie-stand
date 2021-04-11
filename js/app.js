c46e41e0a96fba7606e9047dba96a38417687675
AbdullahElian1 <abodelian28@gmail.com>
2021-04-11 15:03:16 +0300

    complete seattle location

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const seattle ={
     min: 23,
     max: 65,
     avg: 6.3,
+    hours:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
     cusNum: [],
     calCookies:[],
     totalCookies:0,
@@ -27,6 +28,7 @@ const seattle ={
         {
 
             this.calCookies.push(this.cusNum[i]* this.avg);
+            
         }
 
     },
@@ -47,6 +49,20 @@ const seattle ={
         let h2 = document.createElement('h2');
         container.appendChild(h2); 
         h2.textContent = this.locationName;
+        let ul = document.createElement('ul');
+        container.appendChild(ul);
+        let li = null;
+        for(let i = 0 ; i < this.calCookies.length ; i++){
+            li = document.createElement('li');
+            ul.appendChild(li);
+            
+            li.textContent = `${this.hours[i]}: ${this.calCookies[i]} Cookies`;
+        }
+        li = document.createElement('li');
+            ul.appendChild(li);
+            li.textContent = `Tootal: ${this.totalCookies} Cookies`;
+
+
 
     }
 
